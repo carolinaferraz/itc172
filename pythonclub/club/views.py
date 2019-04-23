f10b008f8c6292395d324a5be1038365f2c14b30
carolinaferraz <43863678+carolinaferraz@users.noreply.github.com>
2019-04-23 11:21:07 -0700

    A5 Resource View
    - adds 3 users
    - creates a group called members with a couple of the users added to it
    - adds three resources
    - adds a view that returns all the resources
    - creates a template for the view
    - adds the url for the view
    - adds a link to the view in the base.html

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Resource
 
-# Create your views here.
-from django.shortcuts import render
 
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+#resources 
+def getresources(request):
+    type_list=Resource.objects.all()
+    return render(request, 'club/resources.html' ,{'type_list' : type_list})
